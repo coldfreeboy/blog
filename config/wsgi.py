0e84a63a2a4bf44a86c988aaea18f6dd705cdf73
dang <270768983@qq.com>
2016-06-19 22:56:06 +0800

    init1.1

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,11 +6,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
-
 import os
+import sys
+# env config
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_config.settings")
+# print(sys.path)
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-application = get_wsgi_application()
+if 'SERVER_SOFTWARE' in os.environ: #BAE
+    from bae.core.wsgi import WSGIApplication
+    application = WSGIApplication(get_wsgi_application())
+else:  
+    application = get_wsgi_application()
